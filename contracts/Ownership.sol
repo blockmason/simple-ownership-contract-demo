137111f4bb4ac7f4e145f8fea74a37bcc11d66c7
harishraisinghani <harishraisinghani@gmail.com>
2019-07-18 18:37:15 -0700

    Create new multichain script

--- contracts/Ownership.sol
+++ contracts/Ownership.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.5.8;
 contract Ownership {
     mapping(string => address) public ownerOf;
     address public authority;
-    
+
     constructor() public {
         authority = msg.sender;
     }
-    
+
     function setOwner(string memory asset, address owner) public {
         ownerOf[asset] = owner;
     }
